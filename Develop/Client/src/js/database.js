962f2818aa156e61a1138c58aa5f70ba5d6c1fa9
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-08 16:38:42 -0600

    updated database.js

--- Develop/Client/src/js/database.js
+++ Develop/Client/src/js/database.js
@@ -16,9 +16,9 @@ const initdb = async () =>
 export const putDb = async (content) => { console.error('post to database');
 const dbPromise = await openDB('jate', 1);
 const db = dbPromise;
-const Trans = db.transaction('jate', 'readwrite');
-const Store = Trans.objectStore('jate');
-const request = Store.put({ id: 1, value: content });
+const trans = db.transaction('jate', 'readwrite');
+const store = trans.objectStore('jate');
+const request = store.put({ id: 1, value: content });
 
 const result = await request;
 console.log(result);
@@ -28,9 +28,9 @@ console.log(result);
 export const getDb = async () => {console.error('get from database');
 const dbGet = openDB('jate', 1);
 const db = await dbGet;
-const Trans = db.transaction('jate', 'readonly');
-const Store = Trans.objectStore('jate');
-const request = await Store.get(1);
+const trans = db.transaction('jate', 'readonly');
+const store = trans.objectStore('jate');
+const request = await store.get(1);
 const result = await request;
 console.log(result.value);
 }
