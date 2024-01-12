3bd1e4b20e496a23c2ba85211faf89fe44195c07
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-11 22:12:24 -0600

    debugged

--- Develop/Client/src/js/database.js
+++ Develop/Client/src/js/database.js
@@ -14,17 +14,18 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => { 
-const db = await openDB('jate', 1);
-const trans = db.transaction('jate', 'readwrite');
+console.log("put")
+const jdb = await openDB('jate', 1);
+const trans = jdb.transaction('jate', 'readwrite');
 const store = trans.objectStore('jate');
 const request = store.put({id: 1, value: content });
 const result = await request;
-console.log(result);
+// console.log(result?.value);
 
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (e) => {
+export const getDb = async () => {
 console.log("get")
 const db = await openDB('jate', 1);
 const trans = db.transaction('jate', 'readonly');
